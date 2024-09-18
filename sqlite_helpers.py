b5f2b1949e97418ed674afed00e0a0fb403c7954
iOliver678 <olivermajano01@gmail.com>
2024-09-18 12:53:05 -0700

    first commit

---
+++ sqlite_helpers.py
@@ -0,0 +1,9 @@
+
+#function for table creation
+    #table will have url, alias, timestamp, id
+#function for inserting a url into the table
+#function for deleting a url
+#function for listing all urls
+#function for retrieving 1 url based on alias
+#function for checking if alias exists in the db
+    
