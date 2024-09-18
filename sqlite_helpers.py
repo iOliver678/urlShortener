65a9e7e368e6aefe60f39404d58a4e0bbba5d187
iOliver678 <olivermajano01@gmail.com>
2024-09-18 12:56:16 -0700

    test

--- sqlite_helpers.py
+++ sqlite_helpers.py
@@ -1,4 +1,4 @@
-
+import sqlite3
 #function for table creation
     #table will have url, alias, timestamp, id
 #function for inserting a url into the table
@@ -6,4 +6,15 @@
 #function for listing all urls
 #function for retrieving 1 url based on alias
 #function for checking if alias exists in the db
+db_file = "Database.db"
+db = sqlite3.connect(db_file)
+cursor = db.cursor()
+query = "CREATE TABLE urls(url, alias, timestamp, id)"
+
+def createTable():
+    cursor.execute(query)
+    db.commit
     
+
+    
+
